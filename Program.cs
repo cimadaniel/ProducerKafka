5c1fdddab58dc0fec08c3bb20b87e0d0120a61b2
Daniel Cima <89922721+cimadaniel@users.noreply.github.com>
2023-12-03 21:30:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "trabalho-receitas.servicebus.windows.net:9093",
+            SaslMechanism = SaslMechanism.Plain, // Define o mecanismo de autenticação como Plain
+            SecurityProtocol = SecurityProtocol.SaslPlaintext, // Define o protocolo de segurança como SaslPlaintext
+            SaslUsername = "$ConnectionString", // Substitua pelo seu nome de usuário
+            SaslPassword = "Endpoint=sb://trabalho-", // Substitua pela sua senha
+        };
+
+        using (var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            string topic = "app-receitas";
+
+            var message = new Message<Null, string>
+            {
+                Value = "[Daniel];[daniel@email];[Nova Receita];[Nova receita adicionada ao site]",
+            };
+
+            var deliveryReport = producer.ProduceAsync(topic, message).Result;
+
+            Console.WriteLine($"Enviado para a partição: {deliveryReport.Partition}, Offset: {deliveryReport.Offset}");
+        }
+    }
+}
